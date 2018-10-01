818f13c07123c906c3410af9b0667814a2dcc696
selman-keskin <32268443+selman-keskin@users.noreply.github.com>
2018-10-01 19:17:22 +0300

    Add files via upload

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -34,9 +34,11 @@ namespace ConsoleApp1
                 {
                     var line = reader.ReadLine();
                     var values = line.Split('\t');
-
-                    empList1.Add(new List1() { PLAY_ID = values[0], SONG_ID = values[1], CLIENT_ID = values[2], PLAY_TS = values[3] });
-
+                    var datetime = values[3].Split(' ');
+                    if (datetime[0] == "10/08/2016")
+                    {
+                        empList1.Add(new List1() { PLAY_ID = values[0], SONG_ID = values[1], CLIENT_ID = values[2], PLAY_TS = values[3] });
+                    }
                 }
             }
 
@@ -44,21 +46,45 @@ namespace ConsoleApp1
                   .Select(x => new { CLIENT_ID = x.Key, DISTINCT_PLAY_COUNT = x.Select(y => y.SONG_ID).ToList() })
                   .ToList();
 
-            for (var i = 1; i < groupedDistinct1.Count; i++)
+            //var groupedDistinct1 = empList1.GroupBy(x => x.CLIENT_ID)
+            //      .Select(x => new { CLIENT_ID = x.Key, DISTINCT_PLAY_COUNT = x.Select(y => y.SONG_ID).ToList() })
+            //      .ToList();
+
+
+            //for (var j = 0; j < empList1.Count; j++)
+            //    Console.WriteLine(j + "\t" + empList1[j].CLIENT_ID + "\t" + empList1[j].SONG_ID);
+            //for (var j = 0; j < 15; j++)//groupedDistinct1.Count; j++)
+            //{
+            //    Console.WriteLine(j + "\t" + groupedDistinct1[j].CLIENT_ID + "\t\t\t" + groupedDistinct1[j].DISTINCT_PLAY_COUNT.Distinct().Count());
+            //    for (var k = 0; k < groupedDistinct1[j].DISTINCT_PLAY_COUNT.Count; k++)
+            //        Console.WriteLine("\t" + k + "\t" + groupedDistinct1[j].DISTINCT_PLAY_COUNT[k]);
+            //}
+
+            for (var i = 0; i < groupedDistinct1.Count; i++)
             {
-                empList2.Add(new List2() { CLIENT_ID = groupedDistinct1[i].CLIENT_ID, DISTINCT_PLAY_COUNT = groupedDistinct1[i].DISTINCT_PLAY_COUNT.Count.ToString() });
+                empList2.Add(new List2() { CLIENT_ID = groupedDistinct1[i].CLIENT_ID, DISTINCT_PLAY_COUNT = groupedDistinct1[i].DISTINCT_PLAY_COUNT.Distinct().Count().ToString() });
+                //Console.WriteLine(groupedDistinct1[i].CLIENT_ID +"\t"+ groupedDistinct1[i].DISTINCT_PLAY_COUNT.Distinct().Count().ToString());
+
             }
+
+
+            //for (var j = 0; j < empList2.Count; j++)
+            //    Console.WriteLine(j + "\t" + empList2[j].CLIENT_ID + "\t" + empList2[j].DISTINCT_PLAY_COUNT);
 
             var groupedDistinct2 = empList2.GroupBy(x => x.DISTINCT_PLAY_COUNT)
             .Select(x => new { CLIENT_ID_COUNT = x.Key, DISTINCT_PLAY_COUNT = x.Select(y => y.DISTINCT_PLAY_COUNT).ToList() })
             .ToList();
+
+            //Console.WriteLine("Liste2");
+            //for (var l = 0; l < groupedDistinct2.Count; l++)
+            //    Console.WriteLine(groupedDistinct2[l].CLIENT_ID_COUNT + "\t" + groupedDistinct2[l].DISTINCT_PLAY_COUNT.Count);
 
             using (StreamWriter writetext = new StreamWriter("output.csv"))
             {
                 writetext.WriteLine("DISTINCT_PLAY_COUNT\tCLIENT_COUNT");
                 for (var i = 0; i < groupedDistinct2.Count; i++)
                 {
-                    writetext.WriteLine(groupedDistinct2[i].CLIENT_ID_COUNT + "\t" + groupedDistinct2[i].DISTINCT_PLAY_COUNT.Count);
+                    writetext.WriteLine(groupedDistinct2[i].CLIENT_ID_COUNT + "\t" + groupedDistinct2[i].DISTINCT_PLAY_COUNT.Count());
                 }
             }
 
